2e8b44fbcc94025fd5a3f68143fea288424d8ce3
andrewshortall <andrew.shortall@humanitec.com>
2019-04-17 16:27:15 +0200

    removed product client specific sagas and reducers

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,5 +1,4 @@
 import authSaga from "./Auth.saga"
-import productsSaga from "clients/Products/src/redux/Products.saga"
 import { all } from 'redux-saga/effects'
 
 // single entry point to start all Sagas at once
@@ -7,6 +6,5 @@ export default function* rootSaga() {
   yield all([
     // import all sagas and call them here:
     authSaga(),
-    productsSaga()
   ])
 }
