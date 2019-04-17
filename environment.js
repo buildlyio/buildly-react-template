ee764efaed855117ebaf19791b971ecefe7073fa
mughees <mugheesilyas@live.com>
2019-04-17 13:58:00 +0200

    more webpack updates

--- environment.js
+++ environment.js
@@ -1,4 +1,4 @@
-export const environment = (window as any).environment || {
+export const environment = window.environment || {
     API_URL: 'http://localhost:8090/',
     OAUTH_CLIENT_ID: 'wkXLlC9h3k0jxIx7oLllxpFVU89Dxgi7O8FYZyfX',
     OAUTH_TOKEN_URL:  'http://localhost:8090/oauth/token/',
