12a52a80285aaf6aedb6c67b0701148a9d2eb9b8
andrewshortall <andrew.shortall@humanitec.com>
2019-04-18 11:38:05 +0200

    Imported products to saga/reducer index.js files

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,11 @@
 
 import { combineReducers } from 'redux';
+import productReducer from "clients/Products/src/redux/Products.reducer"
 import authReducer from './Auth.reducer';
 
 const rootReducer = combineReducers({
   authReducer,
+  productReducer
 });
 
 export default rootReducer;
