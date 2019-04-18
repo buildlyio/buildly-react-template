d360e1c06be668dc02840530094dad10c141fab8
mughees <mugheesilyas@live.com>
2019-04-18 15:56:59 +0200

    adding saga and reducers automatically for clients

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,12 +1,11 @@
 import authSaga from "./Auth.saga"
-import productsSaga from "clients/Products/src/redux/Products.saga"
 import { all } from 'redux-saga/effects'
 
 // single entry point to start all Sagas at once
 export default function* rootSaga() {
   yield all([
     // import all sagas and call them here:
-    authSaga(),
-    productsSaga()
+    //entryPointForGulp
+    authSaga()
   ])
 }
