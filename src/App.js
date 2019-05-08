74f77a18f648e0328c8eef33eed9fd18b121eb67
andrewshortall <andrew.shortall@humanitec.com>
2019-05-08 16:00:46 +0200

    modules has been added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { hot } from 'react-hot-loader';
 import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom';
 import { PrivateRoute } from './midgard/routes/Private.route';
@@ -9,25 +9,23 @@ import Container from './midgard/layout/Container/Container';
 import Login from './midgard/pages/Login/Login';
 import Register from './midgard/pages/Register/Register';
 
-class App extends Component{
-  render(){
-    return(
-      <Router>
-        <div className="app">
-          <Route exact path="/" render={() => (
-            oauthService.hasValidAccessToken() ? (
-              <Redirect to="/app"/>
-            ) : (
-              <Redirect to="/login"/>
-            )
-          )}/>
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <PrivateRoute path="/app" component={Container} />
-        </div>
-      </Router>
-    );
-  }
+function App() {
+  return(
+    <Router>
+      <div className="app">
+        <Route exact path="/" render={() => (
+          oauthService.hasValidAccessToken() ? (
+            <Redirect to="/app"/>
+          ) : (
+            <Redirect to="/login"/>
+          )
+        )}/>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+        <PrivateRoute path="/app" component={Container} />
+      </div>
+    </Router>
+  );
 }
 
 export default hot(module)(App);
