1e2a39a694eaf62252aa116d2192801f8eebbea6
andrewshortall <andrew.shortall@humanitec.com>
2019-05-08 16:02:23 +0200

    modules has been added

--- src/redux/sagas/index.js
+++ src/redux/sagas/index.js
@@ -1,3 +1,5 @@
+import productsSaga from 'clients/Products/src/redux/Products.saga'; 
+import documentsSaga from 'clients/Documents/src/redux/Documents.saga'; 
 // react library imports
 import authSaga from "./Auth.saga"
 import { all } from 'redux-saga/effects'
@@ -7,7 +9,9 @@ export default function* rootSaga() {
   yield all([
     // import all sagas and call them here:
       //entryPointForGulpStart
-//entryPointForGulpEnd
+productsSaga(),
+    documentsSaga(),
+    //entryPointForGulpEnd
     authSaga()
   ])
 }
