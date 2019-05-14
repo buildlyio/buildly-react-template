21331c9c62bb5eb0af9c0682fee445b496c161a9
andrewshortall <andrew.shortall@humanitec.com>
2019-05-14 10:15:24 +0200

    modules has been added

--- src/midgard/context/App.context.js
+++ src/midgard/context/App.context.js
@@ -3,7 +3,7 @@ import React from "react"
 //entryPointForGulpStart
 export const app = {
   appTitle: "My App",
-  [{"id":"products","title":"Products","description":"Add products to inventory"},{"id":"documents","title":"Documents","description":"Organizing docs and images"}]
+  modules: [{"id":"products","title":"Products","description":"Add products to inventory"},{"id":"documents","title":"Documents","description":"Organizing docs and images"}]
 };
 //entryPointForGulpEnd
 
