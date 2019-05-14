66be9fb71d3b4eef9d8d798e19de7615448b3d6f
andrewshortall <andrew.shortall@humanitec.com>
2019-05-14 10:04:29 +0200

    modules has been added

---
+++ src/midgard/context/App.context.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+//entryPointForGulpStart
+export const app = {
+	appTitle: 'My App',
+	modules: []
+};
+//entryPointForGulpEnd
+
+export const AppContext = React.createContext(app);
--- src/midgard/context/User.context.js
+++ src/midgard/context/User.context.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { oauthService } from 'midgard/modules/oauth/oauth.service'
 
-
 export const user = oauthService.getOauthUser() ? oauthService.getOauthUser().data : null ;
 
 export const UserContext = React.createContext(user);
