c2c7b9c3b0eb7e9ba0c072f6a659a54de2f679fe
Aziz <aziz@humanitec.com>
2019-05-27 15:52:09 +0200

    push new table design

--- src/midgard/pages/UserManagement/Users/Users.js
+++ src/midgard/pages/UserManagement/Users/Users.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import styled from 'styled-components'
-import { FjButton, FjTable, FjContentSwitcher } from 'freyja-react'
+import { FjButton, FjTable, FjContentSwitcher, FjMenu } from 'freyja-react'
 
 const UsersWrapper = styled.div`
   width: 100%;
@@ -17,25 +17,30 @@ function Users({ location, history }) {
     { label: 'Read-Only', value: 'read-only' },
   ];
 
-
-  const setPermission = () => {
-    return false;
-  };
+  const permissionCellTemplate = (row) => {
+    return <FjContentSwitcher
+      size="small"
+      options={permissions}
+      active={
+        [row.permission, (item) => {
+          row.permission = item;
+        }]}/>
+  }
 
   return (
     <UsersWrapper>
     <FjTable
       columns={[
-        { label: 'Full name', prop: 'name', template: (item) => {return <b>{item}</b>}, flex: '1' },
+        { label: 'Full name', prop: 'name', template: (row) => {return <b>{row.name}</b>}, flex: '1' },
         { label: 'Email', prop: 'email', flex: '2' },
-        { label: 'Last activity', prop: 'activity', template: (item) => {return <small style={{'color': '#aaa'}}>{item}</small>}, flex: '1' },
-        { label: 'Permissions', prop: 'permissions', template: (item) => {return <FjContentSwitcher size="small" options={permissions} active={['admin', setPermission()]} />}, flex: '2' },
+        { label: 'Last activity', prop: 'activity', template: (row) => {return <small style={{'color': '#aaa'}}>{row.activity}</small>}, flex: '1' },
+        { label: 'Permissions', prop: 'permission', template: permissionCellTemplate, flex: '2' },
         { label: 'Actions', prop: 'options', template: () => {return <FjButton variant="secondary" size="small">•••</FjButton>}, flex: '1' },
       ]}
       rows={[
-        { name: 'Arthur Bailey', email: 'arthur-93@example.com', activity: 'Today' },
-        { name: 'Billy West', email: 'billy.west@example.com', activity: 'Yesterday' },
-        { name: 'Douglas Weaver', email: 'douglas-89@example.com', activity: '2 days ago' }
+        { name: 'Arthur Bailey', email: 'arthur-93@example.com', activity: 'Today', permission: 'admin' },
+        { name: 'Billy West', email: 'billy.west@example.com', activity: 'Yesterday', permission: 'guest' },
+        { name: 'Douglas Weaver', email: 'douglas-89@example.com', activity: '2 days ago', permission: 'guest' }
       ]}
     />
   </UsersWrapper>
