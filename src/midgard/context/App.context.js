b184411928e9c9054916eb21fcdfc9b6c8302b60
mughees <mugheesilyas@live.com>
2019-05-29 18:05:56 +0200

    fixing gulp

--- src/midgard/context/App.context.js
+++ src/midgard/context/App.context.js
@@ -3,7 +3,7 @@ import React from "react"
 //entryPointForGulpStart
 export const app = {
   appTitle: "My App",
-  modules: [{"id":"products","title":"Products","description":"Add products to inventory"},{"id":"documents","title":"Documents","description":"Organizing docs and images"},{"id":"blueprint","title":"Blueprint","description":"Example blue print model"}]
+  modules: []
 };
 //entryPointForGulpEnd
 
