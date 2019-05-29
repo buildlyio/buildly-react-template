6d684331a0982c8ec6e4ebe48db65b1011b23d12
Aziz <aziz@humanitec.com>
2019-05-29 15:49:26 +0200

    working users management

--- src/midgard/pages/UserManagement/Users/Users.js
+++ src/midgard/pages/UserManagement/Users/Users.js
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import styled from 'styled-components'
 import { FjButton, FjTable, FjContentSwitcher, FjMenu } from 'freyja-react'
 import Crud, { CrudContext } from 'midgard/modules/crud/Crud';
+import { getCoreGroups } from 'midgard/redux/coregroup/actions/coregroup.actions'
+import { connect } from 'react-redux'
 
 const UsersWrapper = styled.div`
   width: 100%;
@@ -10,28 +12,38 @@ const UsersWrapper = styled.div`
 /**
  * Current users list
  */
-function Users({ location, history }) {
+function Users({ location, history, data, dispatch }) {
 
   // state to toggle actions menus
   const [menuState, setMenuState] = useState({opened: false, id: ''});
+  const [coreGroupsLoaded, setCoreGroupsLoaded] = useState(false);
+  const [permissions, setPermissions] = useState([]);
 
-  // permissions options
-  const permissions = [
-    { label: 'Admin', value: 'admin' },
-    { label: 'Guest', value: 'guest' },
-    { label: 'Read-Only', value: 'Read-Only' },
-  ];
+  useEffect(() => {
+    if (!coreGroupsLoaded) {
+      dispatch(getCoreGroups());
+      setCoreGroupsLoaded(true);
+    } else {
+      // define permissions
+      setPermissions(data.map(coregroup => {
+        return {label: coregroup.name, value: coregroup.id}
+      }))
+    }
+  }, [data]);
+
 
   // table templates
   const permissionsTemplate = (row, crud) => {
-    return <FjContentSwitcher
-      size="small"
-      options={permissions}
-      active={
-        [row.core_groups.name, (item) => {
-          crud.updateItem({id: row.id, core_groups: [{id: 21}]})
-        }]}/>
-  };
+    if (coreGroupsLoaded) {
+      return <FjContentSwitcher
+        size="small"
+        options={permissions}
+        active={
+          [row.core_groups[0] && row.core_groups[0].id || row.core_groups[0], (item) => {
+            crud.updateItem({id: row.id, core_groups: [item]})
+          }]}/>
+    }
+  }
 
   const actionsTemplate = (row, crud) => {
     return <FjMenu
@@ -99,4 +111,6 @@ function Users({ location, history }) {
   )
 }
 
-export default Users;
+const mapStateToProps = (state, ownProps) => ({...state.coreGroupReducer, ...ownProps});
+
+export default connect(mapStateToProps)(Users);
