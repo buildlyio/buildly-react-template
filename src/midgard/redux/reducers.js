d11498ec5671e717e37a4c8d1a9007a6f2257777
Aziz <aziz@humanitec.com>
2019-06-05 16:26:28 +0200

    modules has been added

--- src/midgard/redux/reducers.js
+++ src/midgard/redux/reducers.js
@@ -4,8 +4,6 @@ import authReducer from './authuser/reducers/authuser.reducer';
 import coreuserReducer from './coreuser/coreuser.reducer';
 import coreGroupReducer from './coregroup/reducers/coregroup.reducer'
 
-
-
 const rootReducer = combineReducers(
   {
     //entryPointForGulpStart
