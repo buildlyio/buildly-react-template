4106b9abb5bb4cf882a82b60630d60047cd20a59
Aziz <aziz@humanitec.com>
2019-06-06 12:27:44 +0200

    modules has been added

--- src/midgard/redux/reducers.js
+++ src/midgard/redux/reducers.js
@@ -1,6 +1,6 @@
-import productsReducer from 'clients/Products/src/redux/Products.reducer'; 
-import documentsReducer from 'clients/Documents/src/redux/Documents.reducer'; 
-import blueprintReducer from 'clients/Blueprint/src/redux/Blueprint.reducer'; 
+import productsReducer from './src/clients/Products/src/redux/Products.reducer'; 
+import documentsReducer from './src/clients/Documents/src/redux/Documents.reducer'; 
+import blueprintReducer from './src/clients/Blueprint/src/redux/Blueprint.reducer'; 
 // react library imports
 import { combineReducers } from 'redux';
 import authReducer from './authuser/reducers/authuser.reducer';
