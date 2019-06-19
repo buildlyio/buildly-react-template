098bca91cf9e0eeb95fa04462b8e5f4cf56db3dc
mughees <mugheesilyas@live.com>
2019-06-19 15:46:06 +0200

    modules has been added

--- src/midgard/redux/reducers.js
+++ src/midgard/redux/reducers.js
@@ -1,3 +1,6 @@
+import productsReducer from 'clients/Products/src/redux/products.reducer'; 
+import documentsReducer from 'clients/Documents/src/redux/documents.reducer'; 
+import blueprintReducer from 'clients/Blueprint/src/redux/blueprint.reducer'; 
 // react library imports
 import { combineReducers } from 'redux';
 import authReducer from './authuser/reducers/authuser.reducer';
@@ -10,6 +13,9 @@ import crudDataReducer from 'midgard/modules/crud/redux/crud.reducer';
 const rootReducer = combineReducers(
   {
     //entryPointForGulpStart
+    productsReducer,
+    documentsReducer,
+    blueprintReducer,
     //entryPointForGulpEnd
     authReducer,
     coreuserReducer,
