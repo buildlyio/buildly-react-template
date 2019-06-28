a803e6338284eb10a0876d2039cc5f6f251223a9
mughees <mugheesilyas@live.com>
2019-06-28 16:28:55 +0200

    mono repo commit

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = function (api) {
+    api.cache(true);
+
+    const presets = ["@babel/env", "@babel/preset-react"];
+
+    const plugins =  ["@babel/plugin-transform-runtime"];
+
+    return {
+        presets,
+        plugins
+    };
+}
--- src/midgard/redux/reducers.js
+++ src/midgard/redux/reducers.js
@@ -1,4 +1,3 @@
-import blueprintReducer from 'clients/Blueprint/src/redux/blueprint.reducer'; 
 // react library imports
 import { combineReducers } from 'redux';
 import authReducer from './authuser/reducers/authuser.reducer';
@@ -11,7 +10,6 @@ import crudDataReducer from 'midgard/modules/crud/redux/crud.reducer';
 const rootReducer = combineReducers(
   {
     //entryPointForGulpStart
-    blueprintReducer,
     //entryPointForGulpEnd
     authReducer,
     coreuserReducer,
