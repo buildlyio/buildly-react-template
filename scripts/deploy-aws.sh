ef8be478de49fe6f0b9bc5c33acd40c843af0c4a
Greg Lind <gwlind@gmail.com>
2020-05-11 12:42:00 -0400

    update repo

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -6,5 +6,5 @@ pip install awscli
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 684870619712.dkr.ecr.us-west-2.amazonaws.com
 
 # update latest version
-docker tag transparent-path/buildly-core:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/buildly-core:latest
-docker push 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/buildly-core:latest
+docker tag $DOCKER_REPO:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/$DOCKER_REPO:latest
+docker push 684870619712.dkr.ecr.us-west-2.amazonaws.com/$DOCKER_REPO:latest
