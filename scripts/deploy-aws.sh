7436645e7f84a28ff00e69b70085a4dc1831c168
Greg Lind <gwlind@gmail.com>
2020-05-24 19:52:45 -0400

    add move build to bash script

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -6,5 +6,5 @@ pip install awscli
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 684870619712.dkr.ecr.us-west-2.amazonaws.com
 
 # update latest version
-docker tag transparent-path/sensor_service:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/buildly_react_template:latest
-docker push 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/buildly_react_template:latest
+docker tag transparent-path/sensor_service:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/react-ui:latest
+docker push 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/react-ui:latest
