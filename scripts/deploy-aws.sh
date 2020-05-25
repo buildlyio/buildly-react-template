f4108d2240dbd0d407c4b5491cb532f98092764b
Greg Lind <gwlind@gmail.com>
2020-05-25 09:27:06 -0400

    update script for aws deploy

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -2,7 +2,7 @@
 
 # Push image to ECR
 ###################
-pip install awscli
+pip install --user awscli
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 684870619712.dkr.ecr.us-west-2.amazonaws.com
 
 # update latest version
