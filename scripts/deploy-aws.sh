ce30c381afbf2a9a1c879e3916dc7ec2058942ab
Greg Lind <gwlind@gmail.com>
2020-05-25 08:46:44 -0400

    update script for aws deploy

--- scripts/deploy-aws.sh
+++ scripts/deploy-aws.sh
@@ -2,9 +2,9 @@
 
 # Push image to ECR
 ###################
-pip install -user awscli
+pip install awscli
 aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 684870619712.dkr.ecr.us-west-2.amazonaws.com
 
 # update latest version
-docker tag transparent-path/sensor_service:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/react-ui:latest
+docker tag transparent-path/react-ui:latest 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/react-ui:latest
 docker push 684870619712.dkr.ecr.us-west-2.amazonaws.com/transparent-path/react-ui:latest
