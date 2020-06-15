7a51480ffce9d73aff6e3c1e8122d1251c982d4a
Greg Lind <gwlind@gmail.com>
2020-06-15 08:55:59 -0400

    update token test for build

--- environment.js
+++ environment.js
@@ -1,6 +1,6 @@
 export const environment = window.environment || {
     API_URL: 'http://localhost:8080/',
-    OAUTH_CLIENT_ID: 'wkXLlC9h3k0jxIx7oLllxpFVU89Dxgi7O8FYZyfX',
+    OAUTH_CLIENT_ID: 'd2tYTGxDOWgzazBqeEl4N29MbGx4cEZWVTg5RHhnaTdPOEZZWnlmWA==',
     OAUTH_TOKEN_URL:  'http://localhost:8080/oauth/token/',
     production: false
 };
