ddabbb5a854b5148a717f99b165fb2b2096aa8a9
Andrew Shortall <andrew.shortall@freachly.de>
2021-01-20 20:54:02 +0100

    set production true for .prod

--- src/environments/environment.prod.js
+++ src/environments/environment.prod.js
@@ -6,5 +6,5 @@ export const environment = window['environment'] || {
   API_URL,
   OAUTH_CLIENT_ID: "pljxXjxQbtzLbh8pPQG3xLLrW6tC6XghOp2OGCUS",
   OAUTH_TOKEN_URL: `${API_URL}oauth/token/`,
-  production: false,
+  production: true,
 };
