a40b89b5a4f9107c3e437aebdfcc817b7216f043
Andrew Shortall <andrew.shortall@freachly.de>
2021-01-20 20:44:06 +0100

    Edit environment files

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -2,7 +2,7 @@
 
 const API_URL = "http://127.0.0.1:8080/";
 
-export const environment = window.environment || {
+export const environment = window['environment'] || {
   API_URL,
   OAUTH_CLIENT_ID: "pljxXjxQbtzLbh8pPQG3xLLrW6tC6XghOp2OGCUS",
   OAUTH_TOKEN_URL: `${API_URL}oauth/token/`,
