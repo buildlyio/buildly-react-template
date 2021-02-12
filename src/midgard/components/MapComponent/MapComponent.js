2c36aabf0070b304f200d808a8a70bf7e5eee587
Yasmin Ansari <yasmin@ajackus.com>
2021-02-12 16:39:18 +0530

    WIP: Map enhancements

--- src/midgard/components/MapComponent/MapComponent.js
+++ src/midgard/components/MapComponent/MapComponent.js
@@ -4,6 +4,7 @@ import {
   withGoogleMap,
   GoogleMap,
   Marker,
+  InfoWindow,
 } from "react-google-maps";
 const {
   MarkerWithLabel,
@@ -13,9 +14,10 @@ const labelSize = { width: 150 };
 const labelPadding = 8;
 
 export function MapComponent(props) {
-  const { markers } = props;
-// 47.6062° N, 122.3321° W
+  const { markers, zoom } = props;
   const [center, setCenter] = useState({ lat: 47.606209, lng: -122.332069 });
+  const [activeMarker, setActiveMarker] = useState(null);
+  // const [toggleInfo, setToggleInfo] = useState(false);
 
   useEffect(() => {
     if (markers && markers.length && markers[0].lat && markers[0].lng) {
@@ -32,39 +34,68 @@ export function MapComponent(props) {
     }
   };
 
-  return <RenderedMap {...props} onMarkerDrag={onMarkerDrag} center={center} />;
+  const handleToggleOpen = (markerId) => {
+    setActiveMarker(markerId);
+  }
+
+  const handleToggleClose = () => {
+    setActiveMarker(null);
+  }
+
+  return <RenderedMap {...props} onMarkerDrag={onMarkerDrag} center={center} handleToggleOpen={handleToggleOpen}
+  handleToggleClose={handleToggleClose}/>;
 }
 
 const RenderedMap = withScriptjs(
   withGoogleMap((props) => (
-    <GoogleMap defaultZoom={5} defaultCenter={props.center}>
+    <GoogleMap defaultZoom={props.zoom} defaultCenter={props.center}>
       {props.isMarkerShown &&
         props.markers &&
         props.markers.map((mark, index) =>
           mark.label ? (
-            <MarkerWithLabel
-              key={`marker${index}:${mark.lat},${mark.lng}`}
-              position={{ lat: mark.lat, lng: mark.lng }}
-              labelAnchor={new google.maps.Point(0, 0)}
-              icon={{
-                path: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
-                fillColor: `${mark.color}`,
-                fillOpacity: 1,
-                strokeColor: "white",
-                scale: 1.4,
-                anchor: { x: 12, y: 24 },
-              }}
-              labelStyle={{
-                color: '#000',
-                backgroundColor: "#FFFF99",
-                fontSize: "11px",
-                padding: labelPadding + "px",
-                width: labelSize.width + "px",
-                borderRadius: "4px",
-              }}
+            // <MarkerWithLabel
+            //   key={index}
+            //   position={{ lat: mark.lat, lng: mark.lng }}
+            //   labelAnchor={new google.maps.Point(0, 0)}
+            //   icon={{
+            //     path: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
+            //     fillColor: `${mark.color}`,
+            //     fillOpacity: 1,
+            //     strokeColor: "white",
+            //     scale: 1.4,
+            //     anchor: { x: 12, y: 24 },
+            //   }}
+            //   labelStyle={{
+            //     color: '#000',
+            //     backgroundColor: "#FFFF99",
+            //     fontSize: "11px",
+            //     padding: labelPadding + "px",
+            //     width: labelSize.width + "px",
+            //     borderRadius: "4px",
+            //   }}
+            // >
+            //   <span>{mark.label}</span>
+            // </MarkerWithLabel>
+            <Marker
+            key={index}
+            position={{ lat: mark.lat, lng: mark.lng }}
+            label={`Index: ${index.toString()} - ${props.activeMarker}`}
+            icon={{
+              path: "M12 2C8.13 2 5 5.13 5 9c0 5.25 7 13 7 13s7-7.75 7-13c0-3.87-3.13-7-7-7zm0 9.5c-1.38 0-2.5-1.12-2.5-2.5s1.12-2.5 2.5-2.5 2.5 1.12 2.5 2.5-1.12 2.5-2.5 2.5z",
+              fillColor: `${mark.color}`,
+              fillOpacity: 1,
+              strokeColor: "white",
+              scale: 1.4,
+              anchor: { x: 12, y: 24 },
+            }}
+            onClick={props.handleToggleOpen(index)}
             >
-              <span>{mark.label}</span>
-            </MarkerWithLabel>
+              {props.activeMarker === index && (
+                <InfoWindow onCloseClick={props.handleToggleClose}>
+                  <span>{mark.label}</span>
+                </InfoWindow>
+              )}
+          </Marker>
           ) : (
             <Marker
               draggable={mark.draggable}
--- src/midgard/pages/Shipment/Shipment.js
+++ src/midgard/pages/Shipment/Shipment.js
@@ -117,6 +117,7 @@ function Shipment(props) {
   const [filteredRows, setFilteredRows] = useState([]);
   const [mapShipmentFilter, setMapShipmentFilter] = useState(null);
   const [markers, setMarkers] = useState([]);
+  const [zoomLevel,setZoomLevel] = useState(5);
   const [tileView, setTileView] = useState(true);
   const [isMapLoaded,setMapLoaded] = useState(false);
   let routesInfo = [];
@@ -297,7 +298,235 @@ function Shipment(props) {
         return obj.supported_class === "Temperature";
       })[0]["name"].toLowerCase()
       let tempConst = temperatureUnit[0].toUpperCase()
-      mapShipmentFilter.sensor_report.forEach((report) => {
+      let sampleLocationData = [
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/3278/",
+          "id": 3278,
+          "report_uuid": "fbfc22fe-bdab-493b-8670-4f94caadc480",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "21.5",
+          "report_humidity": "39",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.604967, 'longitude': -122.059516, 'altitude': 79.5, 'positionUncertainty': 47.423622, 'locationMethod': 'GPS', 'timeOfPosition': 1613114116490}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-12T07:17:02.780879Z",
+          "edit_date": "2021-02-12T07:17:02.780888Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1743/",
+          "id": 1743,
+          "report_uuid": "252171c3-fba7-49a5-bfc9-dd95704c3540",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.6",
+          "report_humidity": "48",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.605091, 'longitude': -122.059825, 'altitude': 66.1, 'positionUncertainty': 53.15073, 'locationMethod': 'GPS', 'timeOfPosition': 1612178142786}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T11:30:00.690088Z",
+          "edit_date": "2021-02-01T11:30:00.690097Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1744/",
+          "id": 1744,
+          "report_uuid": "ed73c6c4-d702-4c94-a8a9-a6691e71af69",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.5",
+          "report_humidity": "48",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.604944, 'longitude': -122.059725, 'altitude': 137.9, 'positionUncertainty': 31.890438, 'locationMethod': 'GPS', 'timeOfPosition': 1612179040967}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T11:45:00.627719Z",
+          "edit_date": "2021-02-01T11:45:00.627728Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1745/",
+          "id": 1745,
+          "report_uuid": "f91c1655-4e88-452c-88e3-f4bc05770791",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.4",
+          "report_humidity": "49",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.605049, 'longitude': -122.059656, 'altitude': 107.8, 'positionUncertainty': 41.617306, 'locationMethod': 'GPS', 'timeOfPosition': 1612179980123}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T12:00:00.691841Z",
+          "edit_date": "2021-02-01T12:00:00.691852Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1746/",
+          "id": 1746,
+          "report_uuid": "56dd1801-3af0-4789-a3c5-642c30e56907",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.4",
+          "report_humidity": "47",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.604828, 'longitude': -122.059612, 'altitude': 107.5, 'positionUncertainty': 34.713108, 'locationMethod': 'GPS', 'timeOfPosition': 1612180845319}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T12:15:00.619211Z",
+          "edit_date": "2021-02-01T12:15:00.619220Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1747/",
+          "id": 1747,
+          "report_uuid": "1d04d168-7900-4136-acc4-60a1e3ea3625",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.3",
+          "report_humidity": "47",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.605227, 'longitude': -122.059882, 'altitude': 84, 'positionUncertainty': 56.859474, 'locationMethod': 'GPS', 'timeOfPosition': 1612181766424}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T12:30:00.698845Z",
+          "edit_date": "2021-02-01T12:30:00.698853Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1748/",
+          "id": 1748,
+          "report_uuid": "3012fb9b-fc5c-4704-9f95-3474ba7bb6a2",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.3",
+          "report_humidity": "48",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.604852, 'longitude': -122.059576, 'altitude': 110.3, 'positionUncertainty': 35.805027, 'locationMethod': 'GPS', 'timeOfPosition': 1612182649596}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T12:45:00.620736Z",
+          "edit_date": "2021-02-01T12:45:00.620746Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1749/",
+          "id": 1749,
+          "report_uuid": "2bf5e6e5-35af-4066-88b7-88823d459533",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.2",
+          "report_humidity": "48",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.605115, 'longitude': -122.05955, 'altitude': 47.8, 'positionUncertainty': 59.5063, 'locationMethod': 'GPS', 'timeOfPosition': 1612183565677}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T13:00:00.680729Z",
+          "edit_date": "2021-02-01T13:00:00.680738Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        },
+        {
+          "url": "http://tp-dev-sensors.buildly.io/sensor_report/1750/",
+          "id": 1750,
+          "report_uuid": "04b6428b-899c-48a5-8209-8ab26dfa1b24",
+          "shipment_id": [
+            "120"
+          ],
+          "custodian_id": null,
+          "excursion_flag": false,
+          "warning_flag": false,
+          "mac_address": null,
+          "activation_date": null,
+          "report_temp": "20.2",
+          "report_humidity": "48",
+          "report_frequency": null,
+          "report_location": [
+            "{'latitude': 47.604972, 'longitude': -122.059515, 'altitude': 102.3, 'positionUncertainty': 35.510563, 'locationMethod': 'GPS', 'timeOfPosition': 1612184440882}"
+          ],
+          "report_date_time": null,
+          "create_date": "2021-02-01T13:15:00.657987Z",
+          "edit_date": "2021-02-01T13:15:00.657997Z",
+          "organization_uuid": null,
+          "sensor": null,
+          "gateway": "http://tp-dev-sensors.buildly.io/gateway/51/"
+        }
+      ]
+      sampleLocationData.forEach((report) => {
+      // mapShipmentFilter.sensor_report.forEach((report) => {
         if (report.report_location != null && Array.isArray(report.report_location)) {
           try {
             // data uses single quotes which throws an error
@@ -327,6 +556,7 @@ function Shipment(props) {
         }
       });
       setMarkers(markersToSet);
+      setZoomLevel(12);
     }
   }, [mapShipmentFilter]);
 
@@ -434,6 +664,7 @@ function Shipment(props) {
             isMarkerShown={isMapLoaded}
             markers={markers}
             googleMapURL={MAP_API_URL}
+            zoom={zoomLevel}
             loadingElement={<div style={{ height: `100%` }} />}
             containerElement={<div style={{ height: `550px` }} />}
             mapElement={<div style={{ height: `100%` }} />}
