92aed1188128fb3678866ba7bb8fc29defc34744
vishalajackus <73515569+vishalajackus@users.noreply.github.com>
2021-09-27 15:06:36 +0530

    Update environment.dev.js

--- src/environments/environment.dev.js
+++ src/environments/environment.dev.js
@@ -1,6 +1,6 @@
 // Configuration for development environment
 
-const API_URL = 'http://localhost:8080/';
+const API_URL = 'http://34.149.57.55/';
 const GITHUB_CLIENT_ID = '076e9a822c235db9057f';
 const FEEDBACK_SHEET =
   'https://sheet.best/api/sheets/fd4d0563-683c-4f3f-813c-526b5dc72606';
