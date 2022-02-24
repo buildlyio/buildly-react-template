c5a304d26e2be69863c74e9ebcc1dbff3d12a987
Yasmin Ansari <yasmin@ajackus.com>
2022-02-24 18:13:09 +0530

    Allow edit even if no gateways are attahced (#297)
    Co-authored-by: Yasmin Ansari <yasmin.ansari@ajackus.com>

--- src/pages/Shipment/components/ShipmentInfo.js
+++ src/pages/Shipment/components/ShipmentInfo.js
@@ -322,6 +322,16 @@ const ShipmentInfo = (props) => {
             attachedGateway[0],
           ),
         );
+      } else {
+        dispatch(
+          editShipment(
+            shipmentFormValue,
+            history,
+            `${routes.SHIPMENT}/edit/:${editData.id}`,
+            organization,
+            null,
+          ),
+        );
       }
     } else {
       dispatch(addShipment(shipmentFormValue, history, null, organization));
