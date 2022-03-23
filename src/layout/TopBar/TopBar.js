bcc0fdea0bd92c19b88c36a1c77d8f595a00c36a
MounikaReddy15 <mounikareddygonae15@gmail.com>
2022-03-23 17:24:59 +0530

    remove global admin filter changes

--- src/layout/TopBar/TopBar.js
+++ src/layout/TopBar/TopBar.js
@@ -1,18 +1,15 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import makeStyles from '@mui/styles/makeStyles';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import { TextField, MenuItem } from '@mui/material';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import GroupIcon from '@mui/icons-material/Group';
 import logo from '@assets/topbar-logo.png';
 import { logout } from '@redux/authuser/authuser.actions';
 import { routes } from '@routes/routesConstants';
-import { UserContext } from '@context/User.context';
-import { hasGlobalAdminRights, hasAdminRights } from '@utils/permissions';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
@@ -32,51 +29,18 @@ const useStyles = makeStyles((theme) => ({
   paper: {
     border: '1px solid',
   },
-  globalFilter: {
-    width: theme.spacing(24),
-    marginTop: theme.spacing(1.5),
-    '& .MuiOutlinedInput-input': {
-      padding: theme.spacing(1, 3.5, 1, 2),
-    },
-    '& label.MuiInputLabel-outlined': {
-      color: '#fff',
-    },
-    '& label.Mui-focused': {
-      color: '#fff',
-    },
-    '&:hover': {
-      '&& fieldset': {
-        borderColor: '#fff',
-      },
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: '#fff',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: '#fff',
-      },
-      '& .MuiSelect-icon': {
-        color: '#fff',
-      },
-    },
-  },
 }));
 
 /**
  * Component for the top bar header.
  */
-function TopBar({
+const TopBar = ({
   history,
   location,
   dispatch,
-}) {
+}) => {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const user = useContext(UserContext);
-  let isSuperAdmin = false;
-  const isAdmin = hasAdminRights(user) || hasGlobalAdminRights(user);
-  isSuperAdmin = hasGlobalAdminRights(user);
 
   const handleLogoutClick = () => {
     dispatch(logout());
@@ -91,40 +55,19 @@ function TopBar({
         </Link>
 
         <div className={classes.menuRight}>
-          {isSuperAdmin && (
-          <TextField
-            className={classes.globalFilter}
-            variant="outlined"
-            fullWidth
-            id="org"
-            label="Organization"
-            select
-          >
-            <MenuItem>
-              None
-            </MenuItem>
-          </TextField>
-          )}
-          {isAdmin && (
-            <Link to={routes.USER_MANAGEMENT}>
-              <IconButton aria-label="user-management" color="inherit" size="large">
-                <GroupIcon fontSize="large" className={classes.menuIcon} />
-              </IconButton>
-            </Link>
-          )}
-          <IconButton
-            aria-label="logout"
-            color="inherit"
-            onClick={handleLogoutClick}
-            size="large"
-          >
+          <Link to={routes.USER_MANAGEMENT}>
+            <IconButton aria-label="user-management" color="inherit">
+              <GroupIcon fontSize="large" className={classes.menuIcon} />
+            </IconButton>
+          </Link>
+          <IconButton aria-label="logout" color="inherit" onClick={handleLogoutClick}>
             <ExitToAppIcon fontSize="large" className={classes.menuIcon} />
           </IconButton>
         </div>
       </Toolbar>
     </AppBar>
   );
-}
+};
 
 const mapStateToProps = (state, ownProps) => ({
   ...ownProps,
