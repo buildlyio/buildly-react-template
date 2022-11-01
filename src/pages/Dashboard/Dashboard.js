3897c98c2fbf6ea64c2d484e2a5920e42a4558b7
Radhika Patel <radhika.patel@ajackus.com>
2022-11-01 13:07:51 +0530

    help text for first time

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,10 +1,21 @@
 /* eslint-disable no-nested-ternary */
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
-import { Grid, Typography } from '@mui/material';
+import { Button, Typography } from '@mui/material';
+import { makeStyles } from '@mui/styles';
 import Loader from '@components/Loader/Loader';
 import { routes } from '@routes/routesConstants';
 import UserDashboard from './components/UserDashboard';
+
+const useStyles = makeStyles((theme) => ({
+  firstTimeMessage: {
+    textAlign: 'center',
+    padding: '25% 10%',
+  },
+  button: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const Dashboard = ({
   history,
@@ -12,13 +23,16 @@ const Dashboard = ({
   loaded,
   user,
 }) => {
+  const classes = useStyles();
+  const [route, setRoute] = useState(routes.DASHBOARD);
+
   useEffect(() => {
     if (loaded && !user.survey_status) {
       if (user.user_type === 'Developer') {
-        history.push(routes.DEVELOPER_FORM);
+        setRoute(routes.DEVELOPER_FORM);
       }
       if (user.user_type === 'Product Team') {
-        history.push(routes.NEW_PRODUCT);
+        setRoute(routes.NEW_PRODUCT);
       }
     }
   }, [user]);
@@ -26,6 +40,26 @@ const Dashboard = ({
   return (
     <>
       {loading && <Loader open={loading} />}
+
+      {loaded && user && !user.survey_status && (
+        <div className={classes.firstTimeMessage}>
+          <Typography variant="h6" component="h6">
+            Thanks for registering. To get you started we want to take your through a new product
+            wizard. This will help you get oriented with the system, and create your first product
+            with Inisights!
+          </Typography>
+
+          <Button
+            variant="contained"
+            type="button"
+            className={classes.button}
+            onClick={(e) => history.push(route)}
+          >
+            Let's get started!
+          </Button>
+        </div>
+      )}
+
       {loaded && user && user.survey_status && <UserDashboard history={history} />}
     </>
   );
