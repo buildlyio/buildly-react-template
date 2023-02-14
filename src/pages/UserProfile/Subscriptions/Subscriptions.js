6db7f5c232dbd07b9b7595e192b4fe19ba1a8b7d
IvyMM <ivy.mwonge@gmail.com>
2023-02-14 14:37:56 +0300

    List user subscriptions

--- src/pages/UserProfile/Subscriptions/Subscriptions.js
+++ src/pages/UserProfile/Subscriptions/Subscriptions.js
@@ -1,44 +1,66 @@
 import React, { useContext, useState } from 'react';
 import { UserContext } from '@context/User.context';
-import Table from 'react-bootstrap/Table';
 import {
-  TableBody, TableCell, TableContainer, TableHead, TableRow,
+  Button,
+  Grid,
 } from '@mui/material';
+import MUIDataTable from 'mui-datatables';
 
 const Subscriptions = ({ dispatch }) => {
   // Initialize variables
   const user = useContext(UserContext);
   console.log('user : ', user);
 
+  const columns = [
+    { name: 'subscription_uuid', options: { display: false, filter: false, sort: false } },
+    { name: 'stripe_card_id', options: { display: false, filter: false, sort: false } },
+    { name: 'stripe_product', label: 'Product', options: { filter: true, sort: true } },
+    { name: 'trial_start_date', label: 'Trial start date', options: { filter: true, sort: true } },
+    { name: 'trial_end_date', label: 'Trial end date', options: { filter: true, sort: true } },
+    { name: 'subscription_start_date', label: 'Subscription start date', options: { filter: true, sort: true } },
+    { name: 'subscription_end_date', label: 'Subscription end date', options: { filter: true, sort: true } },
+    {
+      name: '',
+      label: '',
+      options: {
+        filter: false,
+        sort: false,
+        empty: true,
+        customBodyRenderLite: (dataIndex) => (
+          <>
+            <Button
+              type="submit"
+              variant="outlined"
+              color="primary"
+              size="small"
+            >
+              Renew
+            </Button>
+          </>
+        ),
+      },
+    },
+  ];
+
+  const options = {
+    responsive: 'standard',
+    selectableRows: 'none',
+    download: false,
+    search: false,
+    print: false,
+    filter: false,
+    viewColumns: false,
+  };
+
   return (
     <>
-      <TableContainer>
-        <Table sx={{ minWidth: 650 }} aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>Product</TableCell>
-              <TableCell>Trial start date</TableCell>
-              <TableCell>Trial end date</TableCell>
-              <TableCell>Subscription start date</TableCell>
-              <TableCell>Subscription end date</TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {user.subscriptions.map((row) => (
-              <TableRow
-                key={row.stripe_card_id}
-                sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-              >
-                <TableCell>{row.stripe_product}</TableCell>
-                <TableCell>{row.trial_start_date}</TableCell>
-                <TableCell>{row.trial_end_date}</TableCell>
-                <TableCell>{row.subscription_start_date}</TableCell>
-                <TableCell>{row.subscription_end_date}</TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
+      <Grid item xs={12}>
+        <MUIDataTable
+          data={user.subscriptions}
+          columns={columns}
+          options={options}
+        />
+      </Grid>
     </>
   );
 };
