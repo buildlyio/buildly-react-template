ad2a6f588026032064e81c0340aab16a23f95a50
Tanmay Jain <152588390+TanmayJain92@users.noreply.github.com>
2024-05-09 13:30:43 +0530

    Fix battery level in shipment section (#443)
    * Fix battery level in shipment section
    
    * fix issues
    
    * fix notification service pointing
    
    ---------
    
    Co-authored-by: TanmayJain92 <tanmay.jain@ajackus.com>
    Co-authored-by: Radhika Patel <radhika.patel2206@gmail.com>

--- src/react-query/queries/notifications/getVersionNotesQuery.js
+++ src/react-query/queries/notifications/getVersionNotesQuery.js
@@ -4,7 +4,7 @@ export const getVersionNotesQuery = async (versionNumber, displayAlert) => {
   try {
     const response = await httpService.makeRequestWithoutHeaders(
       'get',
-      `https://demo-notification.tpath.io/version_notes/?version_number=v${versionNumber}`,
+      `${window.env.NOTIFICATION_URL}version_notes/?version_number=v${versionNumber}`,
     );
     return response.data;
   } catch (error) {
